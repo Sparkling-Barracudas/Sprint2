f0309dac1e0fa00709b4a12939b0f30133a6cea9
admin <jarell38@msudenver.edu>
2022-02-05 09:23:02 -0700

    code to Update and delete csv file

--- Crud.java
+++ Crud.java
@@ -7,6 +7,7 @@
 
 import java.io.*;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Crud {
 
@@ -22,12 +23,64 @@ public class Crud {
 
     }
 
-    public void update() {
+    /**
+     * uses franck search mothod to find the row and the updates the count of the item (for now)
+     * @param prodId
+     */
+    public void update(String id) {
+       
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String data[] = line.split(",");
+            String returnLine;
+            String current = data[0];
+            if (current == id) {
+               int quantity = data[1];
+               double wholesale = data[2];
+               double salePrice = data[3];
+               String  supId = data[4];
+               returnLine = current + quantity + wholesale + salePrice + supId;
 
+               Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+               System.out.println("Enter New Amount");
+       
+               int newData = myObj.nextInt();  // Read user input
+               data[1] = newData;// sets the newData to quantity collum
+               break;
+            }
+        sc.close();
+        return returneLine;
+
+        
     }
 
-    public void delete() {
+
+    /**
+     * replaces the entire row with a symbol that will notify the program to ignore that row
+     * @param prodId
+     */
+    public void delete(String prodId) {
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String data[] = line.split(",");
+            String returnLine;
+            String current = data[0];
+            if (current == id) {
+               int quantity = data[1];
+               double wholesale = data[2];
+               double salePrice = data[3];
+               String  supId = data[4];
+               data[0] = "#";
+               data[1] = "#";
+               data[2] = "#";
+               data[3] = "#";
+               data[4] = "#";
+               returnLine = current + quantity + wholesale + salePrice + supId;
+               break;
+            }
+        sc.close();
+        return returneLine;
 
     }
-
 }
