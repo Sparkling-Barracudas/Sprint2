f4768b106c4b4901a7851e672a41fc2b4fdc32a9
CrusaderCoder73 <mbustam9@msudenver.edu>
2022-02-23 22:21:54 -0700

    Added final documentation and changes

--- CrudTest.java
+++ CrudTest.java
@@ -1,11 +1,10 @@
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
+
 import java.sql.Statement;
 
 //import java.util.*;
@@ -13,52 +12,45 @@ import org.junit.jupiter.api.Test;
 
 class CrudTest {
 	/**
-	 * A Test Method that tests that the 
-	 * Create method is adding the id to the database
+	 * A Test Method that tests that the Create method is adding the id to the
+	 * database Set the product ID to 732 for the test to work
 	 */
 	@Test
 	void testCreateMethod() {
 
-		
 		Crud crud = Crud.getInstance();
 		Connection c = crud.connect();
 		crud.create(c);
 
-		 try {
-			 
-			 	String productId = "732";
-	            Statement stmt = null;
-	            c.setAutoCommit(false);
-	            stmt = c.createStatement();
+		try {
 
-	            ResultSet rs = stmt.executeQuery("Select * FROM Products WHERE product_id = '" + productId + "';");
+			String productId = "732";
+			Statement stmt = null;
+			c.setAutoCommit(false);
+			stmt = c.createStatement();
 
-	            while (rs.next()) {
-	                String idRet = rs.getString("product_id");
-	                //int quantRet = rs.getInt("quantity");
-	                //float costRet = rs.getFloat("wholesale_cost");
-	                //float saleRet = rs.getFloat("sale_price");
-	                //String supRet = rs.getString("supplier_id");
-	                String expected = "732";
-	                assertEquals(expected, idRet);
+			ResultSet rs = stmt.executeQuery("Select * FROM Products WHERE product_id = '" + productId + "';");
 
-	            }
-	            crud.delete(c, "732");
-	            rs.close();
-	            stmt.close();
+			while (rs.next()) {
+				String idRet = rs.getString("product_id");
+				String expected = "732";
+				assertEquals(expected, idRet);
 
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	            System.exit(1);
-	        }
-		
-		
-		
+			}
+			crud.delete(c, "732");
+			rs.close();
+			stmt.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 
 	}
+
 	/**
-	 * Method that tests the read method
-	 * The method ensures that the read method is gathering the correct data
+	 * Method that tests the read method The method ensures that the read method is
+	 * gathering the correct data
 	 */
 	@Test
 	void testReadMethod() {
@@ -71,12 +63,12 @@ class CrudTest {
 			stmt = c.createStatement();
 			String productId = "5TXAQVSQ1SPS";
 			ResultSet rs = stmt.executeQuery("Select * FROM Products WHERE product_id = '" + productId + "';");
-			while(rs.next()) {
+			while (rs.next()) {
 				String expected = "5TXAQVSQ1SPS";
 				String idRet = rs.getString("product_id");
 				assertEquals(expected, idRet);
 			}
-			
+
 			rs.close();
 			stmt.close();
 		} catch (Exception e) {
@@ -84,7 +76,12 @@ class CrudTest {
 			System.exit(1);
 		}
 	}
-	
+
+	/**
+	 * A method that checks the update method is working properly working make sure
+	 * to add 100 for quantity, 12.5 for wholesale cost, 23.5 for sale price and
+	 * LFWDAWDA for supplier ID
+	 */
 	@Test
 	void testUpdateMethod() {
 		Crud crud = Crud.getInstance();
@@ -96,19 +93,18 @@ class CrudTest {
 			stmt = c.createStatement();
 			String productId = "5TXAQVSQ1SPS";
 			ResultSet rs = stmt.executeQuery("Select * FROM Products WHERE product_id = '" + productId + "';");
-			while(rs.next()) {
-				
+			while (rs.next()) {
 
-				 int quantRet = rs.getInt("quantity");
-                float costRet = rs.getFloat("wholesale_cost");
-                float saleRet = rs.getFloat("sale_price");
-                String supRet = rs.getString("supplier_id");
+				int quantRet = rs.getInt("quantity");
+				float costRet = rs.getFloat("wholesale_cost");
+				float saleRet = rs.getFloat("sale_price");
+				String supRet = rs.getString("supplier_id");
 				assertEquals(100, quantRet);
 				assertEquals(12.5, costRet);
 				assertEquals(23.5, saleRet);
 				assertEquals("LFWDAWDA", supRet);
 			}
-			
+
 			rs.close();
 			stmt.close();
 		} catch (Exception e) {
@@ -116,7 +112,11 @@ class CrudTest {
 			System.exit(1);
 		}
 	}
-	
+
+	/**
+	 * A method that tests that the delete method deletes data from the data bas Be
+	 * sure to enter 737 for product ID
+	 */
 	@Test
 	void testDeleteMethod() {
 		Crud crud = Crud.getInstance();
@@ -128,22 +128,20 @@ class CrudTest {
 			stmt = c.createStatement();
 			String productId = "737";
 			ResultSet rs = stmt.executeQuery("Select * FROM Products WHERE product_id = '" + productId + "';");
-			while(rs.next()) {
-				
+			while (rs.next()) {
 
 				String idRet = rs.getString("product_id");
 				String expected = "737";
 				assertEquals(expected, idRet);
 			}
-			
-			
+
 			rs.close();
 			stmt.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		
+
 		crud.delete(c, "737");
 		try {
 			Statement stmt = null;
@@ -151,12 +149,12 @@ class CrudTest {
 			stmt = c.createStatement();
 			String productId = "737";
 			ResultSet rs = stmt.executeQuery("Select * FROM Products WHERE product_id = '" + productId + "';");
-			
-			if(rs.next() == false) {
-				
+
+			if (rs.next() == false) {
+
 				assertFalse(rs.next());
 				assertEquals(false, rs.next());
-			
+
 			}
 			rs.close();
 			stmt.close();
@@ -164,6 +162,6 @@ class CrudTest {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		
+
 	}
 }
